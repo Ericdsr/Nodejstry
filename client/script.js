0c3ab1b648caf074e608c54648334f0e09899ad4
Ericdsr <eric62.dasilvarocha@laposte.net>
2023-06-09 13:23:42 +0200

    on try tjr

--- client/script.js
+++ client/script.js
@@ -2,24 +2,23 @@ var fetch = document.getElementById('fetcher')
 
 function fetchtry() {
 
-    fetch('http://127.0.0.1/')
-    .then(response => data.json())
-    .then(data => {
-
-        var username = username;
-        var nickname = nickname;
-        
-    data.data.forEach(get => {
-        console.log(data.data)
-        fetch.innerHTML = `<div class="col-sm-6">
-              <div class="card">
-                <div class="card-body aleatoire">
-                  <p class="card-title auteurs"> ${username}</p>
-                  <h6 class="card-text citations"> ${nickname}</h6>
-                </div>
-              </div>
-            </div>`
-    })
-    })
-    .catch(error => console.error(error))
+  fetch('http://localhost:3000/')
+  .then(res => res.text())
+  .then(text => JSON.parse(fs.readFileSync('data.json')))
+  .then(json => console.log(json.pageItems))
 }
+      
+ /* data.data.forEach(get => {
+      console.log(data.data)
+      fetcher.innerHTML = `<div class="col-sm-6">
+            <div class="card">
+              <div class="card-body aleatoire">
+                <p class="card-title auteurs"> ${id}</p>
+                <h6 class="card-text citations"> ${auteurs}</h6>
+              </div>
+            </div>
+          </div>`
+  })
+  })
+  .catch(error => console.error(error))
+} */
--- serveur/script.js
+++ serveur/script.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
 // Route racine
 app.get('/', (req, res) => {
